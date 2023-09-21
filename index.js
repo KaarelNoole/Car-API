5de8f46dd7397d3f395b5bf1163a0c1b96be1134
martinzeroo <martinhobesalu@gmail.com>
2023-09-21 12:23:22 +0300

    Created sellers

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const port = 8080
 const swaggerUI = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+const sellers = [
+    {id: 1, name:"Martin",Email:"martinhobesalu@gmail.com",Phone: "56785234"},
+    {id: 2, name:"Kaarel", Email: "kaarelnoole@gmail.com", Phone:"7563567"}
+]
+
+app.get('/Sellers/:id', (req, res) => {
+    const seller = sellers.find(s => s.id === parseInt(req.params.id))
+    if (!seller) {
+       return(res.status(404).send("Seller with given id was not found"))
+    }
+    res.send(seller)
+})
+app.get('/Sellers', (req, res) => {
+    res.send(sellers)
+})
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.listen(port, () => {
