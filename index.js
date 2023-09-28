d0d6ea53c867a14e6ef62bbb861db5e4057a9806
martinzeroo <martinhobesalu@gmail.com>
2023-09-28 10:37:51 +0300

    Created a list of locations [#186070776]

--- index.js
+++ index.js
@@ -20,3 +20,16 @@ res.send(brands)
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`);
 })
+
+app.get("/Locations",(req,res)=>{
+    res.send(locations.getAll())
+})
+
+app.get("/Locations/:id",(req,res)=>{
+    res.send(locations.getById(req.params.id))
+    if (foundThing === undefined) return res.status(404).send({error: 'location not found'})
+    res.send(foundThing)
+})
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+res.send(locations)
