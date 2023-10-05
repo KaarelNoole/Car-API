b6aa404cf41f4053662c1371391bd64c7b559487
KaarelNoole <kaarelnoole@gmail.com>
2023-10-05 09:54:06 +0300

    add delete brand functionality

--- brands/data.js
+++ brands/data.js
@@ -5,9 +5,25 @@
     ]
 
 exports.getAll = () => {
-    return data.map(b =>{return {"id":b.id,"name":b.name}})
+    return data.map(b =>{return {"id":b.id,"name":b.Brand,"origin":b.origin}})
 }
 
 exports.getById = (id) => {
     return data.find((thing) => thing.id == parseInt(id))
+}
+
+exports.create = (newBrand) => {
+    const newId = Math.max(...data.map((thing) => thing.id)) + 1
+    newBrand.id = newId
+    data.push(newBrand)
+    return newBrand
+}
+
+exports.delete = (id) => {
+    var toBeDelete = this.getById(id)
+    if (toBeDelete === undefined) {
+        return
+    }
+    data = data.filter((e) => toBeDelete.id != e.id)
+    return toBeDelete 
 }
--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const port = 8080
 const swaggerUI = require('swagger-ui-express')
+const yamljs = require('yamljs')
+const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 const brands = require("./brands/data")
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
-app.get("/brands", (req,res) => {
-    res.send(brands.getAll())
-})
-
-app.get("/brands/:id", (req,res) => {
-    const foundThing = brands.getById(req.params.id)
-    if (foundThing === undefined) {
-        res.status(404).send({
-            error: "brand not found"
-        })
-    }
-    res.send(foundThing)
-})    
+//Create
     app.post("/brands", (req, res) => {
         if (!reg.body.Brand || !reg.body.origin) {
             return res.status(400).send({error: "One or all parameter are missing"})
@@ -30,6 +21,33 @@ app.get("/brands/:id", (req,res) => {
         .location(`${getBaseurl(req)}/brands/${createdBrand.id}`)
         .send/createdBrand
     })
+
+//Read
+app.get("/brands", (req,res) => {
+    res.send(brands.getAll())
+})
+
+app.get("/brands/:id", (req,res) => {
+    const foundThing = brands.getById(req.params.id)
+    if (foundThing === undefined) {
+        res.status(404).send({
+            error: "brand not found"
+        })
+    }
+    res.send(foundThing)
+})
+
+//Update
+
+
+//Delete
+    app.delete("/brands/:id", (req, res) => {
+        if (brands.delete(req.params.id)=== undefined ) {
+            return res.status(404).send({error: "Brand not found"})
+        }
+        res.status(204).send()
+    })
+
     function getBaseurl(request){
         return (request.conncetion && request.conncetion.encrypted ? "https": "http")
          + "://" +request.headers.host
