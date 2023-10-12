ef6accfd008fa898387b996bc38a655efdc69417
KaarelNoole <kaarelnoole@gmail.com>
2023-10-12 10:21:24 +0300

    add index

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const port = 8080
 const swaggerUI = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE,process.env.DB_USER,process.env.DB_PASS,{
+    host: process.env.DB_HOST ,
+    dialect: "mariadb"
+})
+
+try {
+     sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 app.use(express.json())
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
