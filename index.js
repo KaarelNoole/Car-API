73cf68922d340fcefea6c6f6237f0a039fc5c896
martinzeroo <martinhobesalu@gmail.com>
2023-10-12 10:10:41 +0300

    Add ORM for database connection

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const port = 8080
 const swaggerUI = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
+const { Sequelize } = require('sequelize');
+const { error } = require('console')
+const sequelize = new Sequelize(process.env.DATABASE,process.env.DB_USER,process.env.PASS, {
+    host: process.env.DB_HOST ,
+    dialect: "mariadb"
+})
 
+try {
+    sequelize.authenticate().then(() => {
+        console.log('Connetion has been established successfully.')
+    });
+}catch (error) {
+    console.error('Unable to connect to the database', error);
+}
 app.use(express.json())
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
@@ -60,3 +73,4 @@ app.get("/prices", (res,req) => {
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`);
 })
+//MartinJaKaarel - parool
