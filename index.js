cf21cc974a66093de1cccce40f2a457a1750c4c8
KaarelNoole <kaarelnoole@gmail.com>
2023-10-12 10:07:07 +0300

    Add ORM on database connection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
 const port = 8080
@@ -5,6 +6,20 @@ const swaggerUI = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml');
 const brands = require("./brands/data")
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE,process.env.DB_USER,process.env.DB_PASS,{
+    host: process.env.DB_HOST ,
+    dialect: "mariadb"
+})
+
+try {
+     sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
 
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
@@ -22,36 +37,7 @@ app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
         .send/createdBrand
     })
 
-//Read
-app.get("/brands", (req,res) => {
-    res.send(brands.getAll())
-})
 
-app.get("/brands/:id", (req,res) => {
-    const foundThing = brands.getById(req.params.id)
-    if (foundThing === undefined) {
-        res.status(404).send({
-            error: "brand not found"
-        })
-    }
-    res.send(foundThing)
-})
-
-//Update
-
-
-//Delete
-    app.delete("/brands/:id", (req, res) => {
-        if (brands.delete(req.params.id)=== undefined ) {
-            return res.status(404).send({error: "Brand not found"})
-        }
-        res.status(204).send()
-    })
-
-    function getBaseurl(request){
-        return (request.conncetion && request.conncetion.encrypted ? "https": "http")
-         + "://" +request.headers.host
-    }
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`);
